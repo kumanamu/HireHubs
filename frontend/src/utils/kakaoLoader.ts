bee4d04dc6c45970cbc3a6f100142439c78ce69b
kumanamu <searchstock1@gmail.com>
2025-12-22 22:57:15 +0900

    최종운영용완성본S

--- frontend/src/utils/kakaoLoader.ts
+++ frontend/src/utils/kakaoLoader.ts
@@ -1,23 +1,24 @@
+// src/page/kakaoLoader.ts
 let kakaoPromise: Promise<void> | null = null;
 
-export const loadKakaoMap = (key: string) => {
+export const loadKakaoMap = () => {
   if (kakaoPromise) return kakaoPromise;
 
   kakaoPromise = new Promise((resolve, reject) => {
-    if (window.kakao?.maps) {
+    if (window.kakao && window.kakao.maps) {
       resolve();
       return;
     }
 
     const script = document.createElement("script");
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${key}&autoload=false`;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAP_JS_KEY}&autoload=false`;
     script.async = true;
 
     script.onload = () => {
       window.kakao.maps.load(() => resolve());
     };
-    script.onerror = reject;
 
+    script.onerror = reject;
     document.head.appendChild(script);
   });
 
